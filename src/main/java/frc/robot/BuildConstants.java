8f0b79b12f25b980f1f90e3a46ed93d1588381a8
Unnas Hussain <uwh.547@gmail.com>
2025-01-18 19:04:41 -0500

    score assist using SelectCommand

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "e97ab6f870ed52a4a8c76e665a2806d1a8805796";
-  public static final String GIT_DATE = "2025-01-16 19:02:24 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-16 19:19:37 EST";
-  public static final long BUILD_UNIX_TIME = 1737073177789L;
+  public static final int GIT_REVISION = 24;
+  public static final String GIT_SHA = "bd0354e22b19b1b97fa1113281b79be8d9db1642";
+  public static final String GIT_DATE = "2025-01-18 19:04:41 EST";
+  public static final String GIT_BRANCH = "score-assist";
+  public static final String BUILD_DATE = "2025-01-18 19:27:33 EST";
+  public static final long BUILD_UNIX_TIME = 1737246453209L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
