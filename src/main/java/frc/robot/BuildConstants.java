461b67c9961751feb7a8595d2832b8815bf03a6d
Unnas Hussain <uwh.547@gmail.com>
2025-01-24 13:27:05 -0500

    build is fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "7204f06339a541abf4740a94ad0b300914f21370";
-  public static final String GIT_DATE = "2025-01-22 20:19:30 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-22 20:20:01 EST";
-  public static final long BUILD_UNIX_TIME = 1737595201914L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "e81860894d274769252eca85c1ecaefb0682fba9";
+  public static final String GIT_DATE = "2025-01-24 12:21:55 EST";
+  public static final String GIT_BRANCH = "rollers-and-mech2d";
+  public static final String BUILD_DATE = "2025-01-24 13:26:43 EST";
+  public static final long BUILD_UNIX_TIME = 1737743203357L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
