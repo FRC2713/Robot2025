cec5b46bdca4b9a32b2e7689ebd3c6517d7a5a2b
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2025-01-27 19:41:25 -0500

    remove kitbot outtake

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "e7d947e2afb6a70cbbdaa87f369640444556339a";
-  public static final String GIT_DATE = "2025-01-24 14:16:17 GMT-05:00";
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "2a7f2208a0d623464345807ee373b4f5c9c4d0c0";
+  public static final String GIT_DATE = "2025-01-27 19:28:51 GMT-05:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 19:26:12 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1738023972824L;
+  public static final String BUILD_DATE = "2025-01-27 19:37:37 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1738024657237L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
