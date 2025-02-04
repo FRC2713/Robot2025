bf694d7b057f20d7d1d7a872abf4910aa919d635
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-02-03 19:51:53 -0500

    pivot kracken

--- src/main/java/frc/robot/subsystems/constants/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/constants/PivotConstants.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.constants;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.util.Units;
@@ -10,7 +13,7 @@ public class PivotConstants {
   public static final int kCANId = 100;
   public static final boolean kInverted = true;
 
-  public static final double kGearing = 200;
+  public static final double kGearing = 48.0 / 18.0;
   public static final double kLength = Units.inchesToMeters(18);
   public static final double kMass = 7.094328; // kg
   public static final double kMinAngle = Units.degreesToRadians(0);
@@ -19,10 +22,14 @@ public class PivotConstants {
   public static final double kRotationConversionFactor = (1.0 / 225.0 * 10); // values from 2024
 
   public static final int kStallCurrentLimit = 30; // amps
+  public static final int kStatorCurrentLimit = 100; // also amps
   public static final double kMaxAngularVelocity = 5; // RPM
 
   // TODO: might need diff controllers for up and down
   public static final ControlGains PID = new ControlGains().p(10).trapezoidal(3.0, 3.0);
+
+  public static final ControlGains motionMagic = new ControlGains().p(42).a(0.01).v(0.01).s(0.01).mmCruiseVelo(0);
+  public static final ControlGains TalonFF = new ControlGains().s(0.).v(0.).a(0.);
 
   public static final double kAbsoluteEncoderOffset = 118.7;
 
@@ -41,6 +48,27 @@ public class PivotConstants {
     config.closedLoop.maxMotion.maxVelocity(kMaxAngularVelocity);
     PID.applyPID(config.closedLoop);
 
+    return config;
+  }
+  public static TalonFXConfiguration createKrakenConfig() {
+    var config = new TalonFXConfiguration();
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.Feedback.SensorToMechanismRatio = PivotConstants.kGearing;
+    config.TorqueCurrent.PeakForwardTorqueCurrent = PivotConstants.kStallCurrentLimit;
+    config.TorqueCurrent.PeakReverseTorqueCurrent = -PivotConstants.kStallCurrentLimit;
+    config.CurrentLimits.StatorCurrentLimit = PivotConstants.kStatorCurrentLimit;
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    config.MotorOutput.Inverted = (PivotConstants.kInverted) ? InvertedValue.CounterClockwise_Positive : InvertedValue.Clockwise_Positive;
+
+    var slot0Config = config.Slot0;
+    slot0Config.kP = PivotConstants.PID.getKP();
+    slot0Config.kI = PivotConstants.PID.getKI();
+    slot0Config.kD = PivotConstants.PID.getKD();
+    slot0Config.kS = PivotConstants.TalonFF.getKS();
+    slot0Config.kV = PivotConstants.TalonFF.getKV();
+    slot0Config.kA = PivotConstants.TalonFF.getKA();
+
+    config.MotionMagic = motionMagic.createMMConfigs();
     return config;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOKrakens.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.pivot;
+
+import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.constants.PivotConstants;
+import frc.robot.subsystems.pivot.PivotIO.PivotInputs;
+import frc.robot.util.PhoenixUtil;
+
+public class PivotIOKrakens {
+    private final TalonFX motor;
+    private double targetDegrees;
+    private final MotionMagicExpoTorqueCurrentFOC angleRequest = new MotionMagicExpoTorqueCurrentFOC(0);
+
+    public PivotIOKrakens() {
+        this.motor = new TalonFX(PivotConstants.kCANId);
+        var config = PivotConstants.createKrakenConfig();
+        PhoenixUtil.tryUntilOk(5, () -> motor.getConfigurator().apply(config, 0.25));
+        PhoenixUtil.tryUntilOk(5, () -> motor.setPosition(PivotConstants.kInitialAngle, 0.25));
+    }
+    public void setVoltage(double volts) {
+        motor.setVoltage(volts);
+    }
+    public void setTargetAngle(double degrees) {
+        this.targetDegrees = degrees;
+        motor.setControl(angleRequest.withPosition(degrees));
+    }
+    public void updateInputs(PivotInputs inputs) {
+        inputs.velocityDPS = motor.getVelocity().getValueAsDouble();
+        inputs.voltage = motor.getMotorVoltage().getValueAsDouble();
+        inputs.isOn =
+            Math.abs(Units.rotationsPerMinuteToRadiansPerSecond(motor.getVelocity().getValueAsDouble()))
+                > 0.005;
+        inputs.angleDegrees = motor.getPosition().getValueAsDouble() * Math.PI * 2;
+        inputs.commandedAngle = targetDegrees;
+    }
+}
