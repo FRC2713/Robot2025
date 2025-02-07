6000ce2c0cd7af4432f7539daeeb68d7fc42033b
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2025-02-07 17:08:14 -0500

    new superstructure

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 90;
-  public static final String GIT_SHA = "2a15583d8c3147c22d503fabcba8ad41abe74a0d";
-  public static final String GIT_DATE = "2025-02-07 16:37:37 GMT-05:00";
+  public static final int GIT_REVISION = 91;
+  public static final String GIT_SHA = "d713563c8eccaeaab8a8375da0a91e968939cf81";
+  public static final String GIT_DATE = "2025-02-07 16:46:33 GMT-05:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-07 16:45:40 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1738964740009L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-07 16:51:43 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1738965103275L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
