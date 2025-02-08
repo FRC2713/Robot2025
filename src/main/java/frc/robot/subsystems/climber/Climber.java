19a5474d73f27e6187f76468d8cbf8093cb5746d
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-02-08 12:54:27 -0500

    Basic climber

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems.pivot;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.constants.PivotConstants;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+public class Climber extends SubsystemBase {
+  private final PivotInputsAutoLogged inputs;
+  private final PivotIO IO;
+  public final MechanismLigament2d mech2d =
+      new MechanismLigament2d(
+          "pivot",
+          PivotConstants.kLength,
+          PivotConstants.kInitialAngleRad,
+          PivotConstants.mech2dWidth,
+          PivotConstants.mech2dColor);
+
+  private double targetAngleDeg = Units.radiansToDegrees(PivotConstants.kInitialAngleRad);
+
+  public Climber(PivotIO IO) {
+    this.inputs = new PivotInputsAutoLogged();
+    IO.updateInputs(inputs);
+    this.IO = IO;
+  }
+
+  public void periodic() {
+    if (PivotConstants.PID.hasChanged(hashCode())) {
+      this.IO.setPID(PivotConstants.PID);
+    }
+
+    IO.updateInputs(inputs);
+    Logger.processInputs("Pivot", inputs);
+  }
+
+  public void setTargetAngle(double degrees) {
+    this.targetAngleDeg = degrees;
+    this.IO.setTargetAngle(degrees);
+  }
+
+  @AutoLogOutput(key = "Pivot/isAtTarget")
+  public boolean isAtTarget() {
+    return Math.abs(this.inputs.angleDegrees - this.targetAngleDeg)
+        < PivotConstants.AT_TARGET_GIVE_DEGS;
+  }
+
+  public void updateMech2D() {
+    // 0 deg points up in Mech2d, +90 points left (or back in 3d)
+    this.mech2d.setAngle((this.inputs.angleDegrees) - 90);
+  }
+
+  public double getCurrentAngle() {
+    return this.inputs.angleDegrees;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/constants/ClimberConstants.java
@@ -0,0 +1,85 @@
+package frc.robot.subsystems.constants;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.util.ControlGains;
+import frc.robot.util.LoggedTunablePID;
+import frc.robot.util.RHRUtil;
+
+public class ClimberConstants {
+  public static final int kCANId = 15;
+  public static final boolean kInverted = false;
+
+  public static final double kGearing = (48.0 / 18.0) * 20;
+  public static final double kLength = Units.inchesToMeters(18);
+  public static final double kMass = 7.094328; // kg
+
+  public static final double kMinAngleRad = Units.degreesToRadians(0);
+  public static final double kMaxAngleRad = Units.degreesToRadians(60);
+
+  public static final double kInitialAngleRad = Units.degreesToRadians(0);
+  public static final double kRampAngleRad = Units.degreesToRadians(30);
+
+  public static final int kStallCurrentLimit = 30; // amps
+  public static final int kStatorCurrentLimit = 100; // also amps
+  public static final double kMaxAngularVelocity = 5; // RPM
+
+  public static final double kP = RHRUtil.modeDependentDouble(300., 10);
+  public static final double kI = 0.0;
+  public static final double kD = RHRUtil.modeDependentDouble(5., 0);
+
+  public static final double kG = RHRUtil.modeDependentDouble(6.5, 0.381);
+  public static final double kV = 0.0;
+  public static final double kA = 0.0;
+  public static final double kS = RHRUtil.modeDependentDouble(40, 0.);
+
+  public static final double KTrapezoidalMaxVelocity = 3;
+  public static final double KTrapezoidalMaxAcceleration = 3;
+
+  public static final LoggedTunablePID PID =
+      new LoggedTunablePID(
+          "Climber",
+          new ControlGains()
+              // PID
+              .p(kP)
+              .i(kI)
+              .d(kD)
+              // FF
+              .g(kG)
+              .s(kS)
+              .v(kV)
+              .a(kA)
+              // Motion Magic
+              .mmCruiseVelo(0),
+          80,
+          1600);
+
+  public static final double kAbsoluteEncoderOffset = 118.7;
+
+  public static final int mech2dWidth = 10;
+  public static final Color8Bit mech2dColor = new Color8Bit(0, 255, 255);
+
+  public static TalonFXConfiguration createKrakenConfig() {
+    var config = new TalonFXConfiguration();
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.Feedback.SensorToMechanismRatio = ClimberConstants.kGearing;
+    config.TorqueCurrent.PeakForwardTorqueCurrent = ClimberConstants.kStallCurrentLimit;
+    config.TorqueCurrent.PeakReverseTorqueCurrent = -ClimberConstants.kStallCurrentLimit;
+    config.CurrentLimits.StatorCurrentLimit = ClimberConstants.kStatorCurrentLimit;
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    config.MotorOutput.Inverted =
+        (ClimberConstants.kInverted)
+            ? InvertedValue.CounterClockwise_Positive
+            : InvertedValue.Clockwise_Positive;
+
+    config.Slot0 = PID.toTalonFX(GravityTypeValue.Arm_Cosine);
+    config.MotionMagic = PID.toMotionMagic();
+    return config;
+  }
+
+  public static final double AT_TARGET_GIVE_DEGS = 2;
+}
