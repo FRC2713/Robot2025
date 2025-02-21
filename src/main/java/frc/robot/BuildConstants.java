12bd6ff6af026a03828cf96fbb549d1b7c28328f
Ty Tremblay <tytremblay@Mac.localdomain>
2025-02-21 15:36:15 -0500

    add shoulder commands

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 124;
-  public static final String GIT_SHA = "583547d2b028b5df38a380e17817855169478185";
-  public static final String GIT_DATE = "2025-02-21 15:31:49 EST";
+  public static final int GIT_REVISION = 125;
+  public static final String GIT_SHA = "35f248d535eebf2a2da13e989b7dd16828a352f4";
+  public static final String GIT_DATE = "2025-02-21 15:35:17 EST";
   public static final String GIT_BRANCH = "add_arm";
-  public static final String BUILD_DATE = "2025-02-21 15:35:03 EST";
-  public static final long BUILD_UNIX_TIME = 1740170103111L;
+  public static final String BUILD_DATE = "2025-02-21 15:36:00 EST";
+  public static final long BUILD_UNIX_TIME = 1740170160842L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
