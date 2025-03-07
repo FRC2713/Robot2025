daf9cc73f5f5c62fc2b29cfca7e031c2fb548fe2
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2025-03-07 10:03:30 -0500

    tuning paths

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 187;
-  public static final String GIT_SHA = "324af9a82e6e29975cba295ddd9c3f99ec184708";
-  public static final String GIT_DATE = "2025-03-07 09:50:05 GMT-05:00";
+  public static final int GIT_REVISION = 188;
+  public static final String GIT_SHA = "9568e99807c753afb845add1524c4f706f8d8513";
+  public static final String GIT_DATE = "2025-03-07 10:01:55 GMT-05:00";
   public static final String GIT_BRANCH = "teleop-assists";
-  public static final String BUILD_DATE = "2025-03-07 09:57:46 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1741359466895L;
+  public static final String BUILD_DATE = "2025-03-07 10:02:11 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1741359731500L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
