68e85e3515811b025c772c8dcd71ccbb0b5f6431
Unnas Hussain <uwh.547@gmail.com>
2025-03-14 12:41:53 -0400

    flipped auto was going to source backwards

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 281;
-  public static final String GIT_SHA = "066e0090884d1e12d40fcebd62d98ae95b573e2e";
-  public static final String GIT_DATE = "2025-03-14 13:54:30 EDT";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-03-14 13:55:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1741974921156L;
+  public static final String GIT_SHA = "80a3de4b583d4a52b901e53fdaf22a042e0a3382";
+  public static final String GIT_DATE = "2025-03-14 10:56:27 EDT";
+  public static final String GIT_BRANCH = "autos_PineTree";
+  public static final String BUILD_DATE = "2025-03-14 12:41:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1741970493465L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
