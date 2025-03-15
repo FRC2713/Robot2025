f60fb3df94b12b5d395a4a1fa638492014379f3f
2713driverstation <frc2713@gmail.com>
2025-03-15 10:40:59 -0400

    Update at 'Sat Mar 15 10:40:59 EDT 2025'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 296;
-  public static final String GIT_SHA = "b5e1833b21d13372f1832f117dc39d5f14be2618";
-  public static final String GIT_DATE = "2025-03-15 10:07:03 EDT";
+  public static final int GIT_REVISION = 297;
+  public static final String GIT_SHA = "a6f18ecc548dfbbff0848c875de4352ef004c772";
+  public static final String GIT_DATE = "2025-03-15 10:12:39 EDT";
   public static final String GIT_BRANCH = "event-maine";
-  public static final String BUILD_DATE = "2025-03-15 10:07:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1742047623997L;
+  public static final String BUILD_DATE = "2025-03-15 10:12:39 EDT";
+  public static final long BUILD_UNIX_TIME = 1742047959795L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
