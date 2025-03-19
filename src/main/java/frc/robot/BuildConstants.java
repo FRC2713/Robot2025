631ded23fad874407c60498f783fd56019b65e18
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-03-18 20:11:04 -0400

    Algae commands

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 319;
+  public static final String GIT_SHA = "6aa2a78cb80832065e313e45680ec6bac888cf37";
+  public static final String GIT_DATE = "2025-03-18 18:36:53 EDT";
+  public static final String GIT_BRANCH = "algae-commands";
+  public static final String BUILD_DATE = "2025-03-18 20:08:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1742342915569L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
