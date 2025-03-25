22e70359eeb53322ec4401ee956fb2be8f702372
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-03-24 20:10:29 -0400

    Make barge score commmand actually score

--- src/main/java/frc/robot/commands/SuperStructure.java
+++ src/main/java/frc/robot/commands/SuperStructure.java
@@ -210,7 +210,8 @@ public enum SuperStructure {
               PivotCmds.setAngle(SSConstants.Pivot.BARGE_ANGLE_DEG),
               ElevatorCmds.setHeightAndWait(SSConstants.Elevator.BARGE_HEIGHT_IN),
               ShoulderCmds.waitUntilAtTarget(),
-              PivotCmds.waitUntilAtTarget()));
+              PivotCmds.waitUntilAtTarget(),
+              AlgaeClawCmds.setSpeed(SSConstants.AlgaeClaw.BARGE_SCORE_SPEED)));
 
   private Supplier<Command> cmd;
 
