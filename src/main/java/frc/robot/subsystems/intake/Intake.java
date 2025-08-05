e0fac37a34d396c2c80b2db7e3e373b413e46303
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-08-04 20:04:42 -0400

    intake updates (WIP)

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//Intake is essentially Shoulder and EndEfffector mashed into one class
+//Intake is essentially Shoulder and EndEfffector mashed into one class- one EndEffector roller + sholder for controling intake angle
 
 public class Intake extends SubsystemBase {
     private final IntakeIO IO;
